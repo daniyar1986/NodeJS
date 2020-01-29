029868b807271ce90731606235d50e37378dd796
DMyrzakanov <Danntu1986#>
2020-01-29 13:30:28 +0600

    Движок V8 и глобальные объекты

--- lesson1.js
+++ lesson1.js
@@ -1,3 +1,14 @@
 console.log("Hello World");
 console.log(__dirname);
 console.log(__filename);
+
+let x = 0;
+
+if (x==0)
+    console.log("Work");
+
+for (let i=0;i<5;i++)
+    console.log(i);
+setTimeout(function () {
+    console.log("hi")
+}, 2000);
