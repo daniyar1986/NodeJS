2ddbc79ebff04fd3713042562fcef23fdb4731a6
DMyrzakanov <Danntu1986#>
2020-01-30 09:35:58 +0600

    Уроки Node JS / #7 - Работа с директориями

--- lesson7.js
+++ lesson7.js
@@ -1,4 +1,30 @@
 const fs = require('fs');
+fs.rmdirSync('new-one');
 fs.mkdirSync('new-one');
+
+fs.mkdir('test',()=>{
+    console.log("Directory created");
+    fs.writeFile('./test/some_new.txt', 'Hello World', ()=>{
+        console.log("file created in folder test");
+    });
+});
+
 fs.renameSync('new-one', 'test');
 fs.rmdirSync('test');
+
+fs.rmdir('new-one', ()=>{
+   console.log("new-one directory deleted");
+});
+
+fs.unlink('world',()=>{
+    console.log("File deleted")
+});
+
+
+
+fs.unlink('./test/some_new.txt',()=>{
+   console.log('deleted file some_new');
+    fs.rmdir('test',()=>{
+       console.log("deleted directory test");
+   });
+});
